10426bb31fb8bd0e6356b50aff7711704c703dfe
trungdev <trungleminh76@gmail.com>
2025-06-16 21:55:43 +0700

    task complete done

---
+++ app/src/main/java/com/example/taskcompleted/MainActivity.kt
@@ -0,0 +1,77 @@
+package com.example.taskcompleted
+
+import android.R.attr.fontWeight
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.taskcompleted.ui.theme.TaskCompletedTheme
+
+class MainActivity : ComponentActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    enableEdgeToEdge()
+    setContent {
+      TaskCompletedTheme {
+        Surface (
+          modifier = Modifier
+            .fillMaxSize()
+            .padding(10.dp)
+        ){
+          Greeting()
+        }
+      }
+    }
+  }
+}
+
+@Composable
+fun Greeting() {
+  Column (
+    modifier = Modifier
+      .fillMaxSize(),
+    horizontalAlignment = Alignment.CenterHorizontally,
+    verticalArrangement = Arrangement.Center
+  ) {
+    Image(
+      painter = painterResource(id = R.drawable.ic_task_completed),
+      contentDescription = null,
+      modifier = Modifier
+        .size(200.dp)
+    )
+    Text(
+      text = "All tasks completed",
+      fontWeight = FontWeight.Bold,
+      modifier = Modifier
+        .padding(top = 24.dp, bottom = 8.dp)
+    )
+    Text(
+      text = "Nice work!",
+      fontSize = 16.sp
+    )
+  }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+  TaskCompletedTheme {
+    Greeting()
+  }
+}
